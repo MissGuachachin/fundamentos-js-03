e44fa0b4ff11e891466fb1d2f78e66841e06d128
Maggie <guachachi02@yahoo.es>
2023-10-31 14:12:59 +0100

    Añadir ejercicio-02

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const mensaje = "Hola a los BlockMakers"
+const mensajeMinuscula = mensaje.toLowerCase()
+
+console.log(mensajeMinuscula)
+
+// Resultado: "hola a los blockmakers"
