c4a295ed75e125a272bcf0a951ddd2a7182e71d0
Maggie <guachachi02@yahoo.es>
2023-10-31 14:14:10 +0100

    Añadir ejercicio-06

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const frase = "Estamos realizando unos ejercicios de JS."
+const palabras = frase.split(" ")
+
+console.log(palabras)
+
+// Resultado: [ 'Estamos', 'realizando', 'unos', 'ejercicios', 'de', 'JS.' ]
