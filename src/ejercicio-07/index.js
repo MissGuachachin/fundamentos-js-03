d53a1fda76eec6c16563b3c4edef10f558b00540
Maggie <guachachi02@yahoo.es>
2023-10-31 14:15:46 +0100

    Añadir ejercicio-07

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const frase = "La ensalada está hecha con patatas"
+const palabraRemplazada = frase.replace("patatas", "batatas")
+
+console.log(palabraRemplazada)
+
+// Resultado: La ensalada está hecha con batatas
