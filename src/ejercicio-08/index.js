9d18fc3c25aebbc5772817b9366faa75476d669b
Maggie <guachachi02@yahoo.es>
2023-11-03 16:55:37 +0100

    Añadir ejercicio-08

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+const frase = "Ser BlockMaker es fantástico"
+const buscaTermino = "BlockMaker"
+const contieneTermino = frase.includes(buscaTermino)
+
+console.log(contieneTermino)
+
+// Resultado: true
