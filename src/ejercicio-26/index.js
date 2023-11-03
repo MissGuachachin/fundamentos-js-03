b05c225dcf3e17805ca02d79286526da4df5049f
Maggie <guachachi02@yahoo.es>
2023-11-03 17:00:18 +0100

    Añadir ejercicio-26

--- src/ejercicio-26/index.js
+++ src/ejercicio-26/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+const array1 = [6, 5, 4]
+const array2 = [3, 2, 1]
+const resultadoComb = array1.concat(array2)
+
+console.log(resultadoComb)
+
+// Resultado: [ 6, 5, 4, 3, 2, 1 ]
