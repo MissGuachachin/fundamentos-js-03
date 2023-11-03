eabe8b29cecf1459c18f196dae2a6b582b18d049
Maggie <guachachi02@yahoo.es>
2023-11-03 17:00:46 +0100

    Añadir ejercicio-28

--- src/ejercicio-28/index.js
+++ src/ejercicio-28/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const numeros = [9, 7, 5, 3, 1]
+const cuadradoNumeros = numeros.map((num) => num * num)
+
+console.log(cuadradoNumeros)
+
+// Resultado: [ 81, 49, 25, 9, 1 ]
