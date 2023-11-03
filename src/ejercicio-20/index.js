5355c19a11b1d0c1f1429ec5baf36327bba1171c
Maggie <guachachi02@yahoo.es>
2023-11-03 16:58:51 +0100

    Añadir ejercicio-20

--- src/ejercicio-20/index.js
+++ src/ejercicio-20/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+const carBrand = ["audi", "benz", "bugatti", "austin"]
+const remCarBrand = carBrand.pop()
+
+console.log(remCarBrand)
+// Resultado: austin
+console.log(carBrand)
+// Resultado: [ 'audi', 'benz', 'bugatti' ]
