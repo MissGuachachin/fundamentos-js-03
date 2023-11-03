1a8ee48861fb7d500091b86441623b23c71bc721
Maggie <guachachi02@yahoo.es>
2023-11-03 17:00:01 +0100

    Añadir ejercicio-25

--- src/ejercicio-25/index.js
+++ src/ejercicio-25/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const resultadoPar = numeros.filter((num) => num % 2 === 0)
+
+console.log(resultadoPar)
+
+// Resultado: [ 2, 4, 6, 8 ]
