e44f1b712717fb168faa4f078bd30629b0b07551
Maggie <guachachi02@yahoo.es>
2023-11-03 16:59:44 +0100

    Añadir ejercicio-24

--- src/ejercicio-24/index.js
+++ src/ejercicio-24/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const frase = "Estos son los ejercicios de JavaScript."
+const resultado = frase.split(" ")
+
+console.log(resultado)
+
+// Resultado: [ 'Estos', 'son', 'los', 'ejercicios', 'de', 'JavaScript.' ]
