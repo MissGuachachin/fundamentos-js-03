34dcc8e236d990d680785a0f64e5c596987e73ce
Maggie <guachachi02@yahoo.es>
2023-11-03 16:59:04 +0100

    Añadir ejercicio-21

--- src/ejercicio-21/index.js
+++ src/ejercicio-21/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const planetas = ["sol", "saturno", "jupiter"]
+planetas.unshift("tierra", "mercurio")
+
+console.log(planetas)
+
+// Resultado: [ 'tierra', 'mercurio', 'sol', 'saturno', 'jupiter' ]
