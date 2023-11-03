78395e1e7832d3573e4f1b87ea7985ec236e704e
Maggie <guachachi02@yahoo.es>
2023-11-03 16:59:18 +0100

    Añadir ejercicio-22

--- src/ejercicio-22/index.js
+++ src/ejercicio-22/index.js
@@ -1,1 +1,8 @@
 // Escribe tu código aquí:
+const flores = ["girasol", "clavel", "rosa", "gladiolo"]
+const remFlores = flores.shift()
+
+console.log(remFlores)
+// Resultado: girasol
+console.log(flores)
+// Resultado: [ 'clavel', 'rosa', 'gladiolo' ]
